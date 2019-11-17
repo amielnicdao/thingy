8836d8ffca2b8ba242acc86252d2af21b17547f9
anicdao <amielnicdao@gmail.com>
2019-11-17 14:15:49 -0500

    added inquire prompt for id, units, and confirm purhcase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var connection = mysql.createConnection( {
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -11,9 +11,42 @@ var connection = mysql.createConnection( {
 });
 
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw (err);
     console.log("Connected as id: " + connection.threadId);
-    connection.end();
-   // start();
+    // connection.end();
+    allItems();
+    //idAndUnit();
 });
+
+function allItems() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw (err);
+        console.log(res);
+        inquirer
+            .prompt([
+                {
+                    name: "id",
+                    type: "input",
+                    message: "What is the ID number of the product you would like to purchase?"
+                },
+                {
+                    name: "unit",
+                    type: "input",
+                    message: "How many units would you like to purchase?"
+            },
+            {
+                name: "purchase",
+                type: "confirm",
+                message: "Would you like to purchase now?"
+            }
+            ])
+            //.then function if user === yes then do this...
+
+    });
+}
+
+
+
+
+

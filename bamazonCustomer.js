06ad324b448730849e06b9f71b52301a3a860e25
anicdao <amielnicdao@gmail.com>
2019-11-19 12:13:14 -0500

    reworked logic, separated allitems from checkquantity function, added switch statement

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,7 +10,6 @@ var connection = mysql.createConnection({
 
 });
 
-
 connection.connect(function (err) {
     if (err) throw (err);
     console.log("Connected as id: " + connection.threadId);
@@ -20,31 +19,95 @@ connection.connect(function (err) {
 });
 
 function allItems() {
-    connection.query("SELECT * FROM products", function (err, res) {
+     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw (err);
-        console.log(res);
-        inquirer
-            .prompt([
+         console.log(res);
+       inquirer
+             .prompt([
+    
                 {
+                    name: "purchase",
+                    type: "list",
+                    message: "Would you like to purchase now?",
+                    choices: ["Yes", "No"]
+                }
+            ]).then(function(answer) {
+                switch(answer.purchase) {
+                    case 'Yes':
+                        // console.log("it works")
+                        checkQuantity();
+                        
+                    break;
+        
+                    case 'No':
+                        connection.end();
+                    break;
+                }
+            })
+            })
+        }
+
+function checkQuantity() {    
+            inquirer
+            .prompt([
+                        {
                     name: "id",
                     type: "input",
-                    message: "What is the ID number of the product you would like to purchase?"
+                    message: "What is the ID number of the product you would like to purchase?",
+                    validate: function(value) {
+                        if (isNaN(value) === false) {
+                          return true;
+                        }
+                        return "Please enter a number."; //how to reset
+                      }
                 },
                 {
                     name: "unit",
                     type: "input",
-                    message: "How many units would you like to purchase?"
-            },
-            {
-                name: "purchase",
-                type: "confirm",
-                message: "Would you like to purchase now?"
-            }
-            ])
-            //.then function if user === yes then do this...
+                    message: "How many units would you like to purchase?",
+                    validate: function(value) {
+                        if (isNaN(value) === false) {
+                          return true;
+                        }
+                        return false;
+                      }
+                }
+            ]).then(function(answer) {
 
-    });
-}
+            // var itemId = answer.id;
+            // var itemUnit = answer.unit;
+
+                connection.query("SELECT * FROM products WHERE ?", answer.id, function(err, res) {
+                    if (err) throw err;
+                        // console.log(res)
+                // for (var i = 0; i < res.length; i++) {
+                    if(answer.unit <= res[0].stock_quantity) {
+                        // console.log("it works")
+                        console.log("Item: " + res[0].product_name);
+                        console.log("Department: " + res[0].department_name);
+                        console.log("Price: " + res[0].price);
+                        console.log("Quantity: " + answer.unit);
+                        console.log("Total: " + res[0].price * answer.unit);
+                    }
+                    else {
+                        if(answer.unit > res[0].stock_quantity) {
+                            console.log("Sorry! We do not have enough in stock. Please try again.")
+                        }
+                    }
+                // }
+
+            }
+            
+                )}
+                   )   }
+        
+        // }
+        // })
+     
+        
+
+    
+
 
 
 

165dadf087ed15b5554716fa78fa2092612c2c9a
anicdao <amielnicdao@gmail.com>
2019-11-19 15:18:11 -0500

    added update database function and new order function. reworked logic again because scope issues, fixed function names

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,102 +13,120 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw (err);
     console.log("Connected as id: " + connection.threadId);
-    // connection.end();
     allItems();
-    //idAndUnit();
 });
 
 function allItems() {
-     connection.query("SELECT * FROM products", function (err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw (err);
-         console.log(res);
-       inquirer
-             .prompt([
-    
+        console.log(res);
+        inquirer
+            .prompt([
                 {
                     name: "purchase",
                     type: "list",
                     message: "Would you like to purchase now?",
                     choices: ["Yes", "No"]
                 }
-            ]).then(function(answer) {
-                switch(answer.purchase) {
+            ]).then(function (answer) {
+                switch (answer.purchase) {
                     case 'Yes':
-                        // console.log("it works")
-                        checkQuantity();
-                        
-                    break;
-        
+                        idAndUnit();
+                        break;
+
                     case 'No':
+                        console.log("Thank you for using Bamazon! See you soon!")
                         connection.end();
-                    break;
+                        break;
                 }
             })
-            })
-        }
+    }
+    )
+}
 
-function checkQuantity() {    
-            inquirer
-            .prompt([
-                        {
-                    name: "id",
-                    type: "input",
-                    message: "What is the ID number of the product you would like to purchase?",
-                    validate: function(value) {
-                        if (isNaN(value) === false) {
-                          return true;
-                        }
-                        return "Please enter a number."; //how to reset
-                      }
-                },
-                {
-                    name: "unit",
-                    type: "input",
-                    message: "How many units would you like to purchase?",
-                    validate: function(value) {
-                        if (isNaN(value) === false) {
-                          return true;
-                        }
-                        return false;
-                      }
+function idAndUnit() {
+    inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "What is the ID number of the product you would like to purchase?",
+                validate: function (value) {
+                    if (isNaN(value) === false) 
+                    {
+                        return true;
+                    }
+                    return "Please enter a number.";  
                 }
-            ]).then(function(answer) {
-
-            // var itemId = answer.id;
-            // var itemUnit = answer.unit;
-
-                connection.query("SELECT * FROM products WHERE ?", answer.id, function(err, res) {
-                    if (err) throw err;
-                        // console.log(res)
-                // for (var i = 0; i < res.length; i++) {
-                    if(answer.unit <= res[0].stock_quantity) {
-                        // console.log("it works")
-                        console.log("Item: " + res[0].product_name);
-                        console.log("Department: " + res[0].department_name);
-                        console.log("Price: " + res[0].price);
-                        console.log("Quantity: " + answer.unit);
-                        console.log("Total: " + res[0].price * answer.unit);
+            },
+            {
+                name: "unit",
+                type: "input",
+                message: "How many units would you like to purchase?",
+                validate: function (value) {
+                    if (isNaN(value) === false) 
+                    {
+                        return true;
                     }
-                    else {
-                        if(answer.unit > res[0].stock_quantity) {
-                            console.log("Sorry! We do not have enough in stock. Please try again.")
-                        }
-                    }
-                // }
-
+                    return false;
+                }
             }
-            
-                )}
-                   )   }
-        
-        // }
-        // })
-     
-        
+        ]).then(function (answer) {
+            connection.query("SELECT * FROM products WHERE item_id=?", answer.id, function (res) {
+                for (var i = 0; i < res.length; i++) {
+                    if (answer.unit > res[i].stock_quantity) {
+                        console.log("Sorry! We do not have enough in stock. Please try again.");
+                        console.log("Item: " + res[i].product_name);
+                        console.log("Department: " + res[i].department_name);
+                        console.log("Price: " + res[i].price);
+                        console.log("Stock: " + res[i].stock_quantity);
+                        newOrder();
+                    } else {
+                        console.log("Item: " + res[i].product_name);
+                        console.log("Department: " + res[i].department_name);
+                        console.log("Price: " + res[i].price);
+                        console.log("Quantity: " + answer.unit);
+                        console.log("Total: " + res[i].price * answer.unit);
 
-    
+                        var updateStock = res[i].stock_quantity - answer.unit;
+                        var itemId = answer.id;
+                        updateDB(updateStock, itemId);
+                    }
+                }
+            });
+        });
+};
 
+function updateDB(updateStock, itemId) {
+    connection.query("UPDATE products SET ? WHERE ?", [{ stock_quantity: updateStock }, { item_id: itemId }],
+        function (err) {
+            if (err) throw err;
+            console.log("Your order has been placed. Thank you for your purchase.");
+            newOrder();
+        }
+    );
+};
 
+function newOrder() {
+    inquirer
+        .prompt({
+            name: "newOrder",
+            type: "list",
+            message: "Would you like to place another order?",
+            choices: ["Yes", "No"]
+        })
+        .then(function (answer) {
+            if (answer.newOrder === "Yes") {
+                console.log("Great! Here are the items.")
+                idAndUnit();
+            } else {
+                connection.end()
+                console.log("Thank you for using Bamazon! See you soon!")
+            }
+        });
+}
+
+//should i create new table
 
 
 
